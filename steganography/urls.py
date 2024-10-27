a7c4e3f6bc75a2016fabb83250c6f16f922d30e8
Thejas M <thejas0123@gmail.com>
2024-10-27 18:14:40 +0530

    made some changes in view messages

--- steganography/urls.py
+++ steganography/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import send_message, view_messages, delete_message, register, user_login, home,base, user_logout
+from .views import send_message, view_messages, delete_message, register, user_login, home,base, user_logout,decode_message
 
 urlpatterns = [
     path('send-message/', send_message, name='send_message'),
-    path('view-messages/', view_messages, name='view_messages'),
-    path('delete-message/<int:message_id>/', delete_message, name='delete_message'),
+    path('view_messages/', view_messages, name='view_messages'),
+    path('decode_message/<int:message_id>/', decode_message, name='decode_message'),
+    path('delete_message/<int:message_id>/', delete_message, name='delete_message'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('home/', home, name='home'),
